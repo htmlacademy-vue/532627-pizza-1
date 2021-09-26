08ab1c9e4b87939aaa05b7b1e970a0f68ac860ac
Mikhail Prokudin <mirex.doors@mail.ru>
2021-09-26 19:05:11 +0700

    another gpg sign

--- src/frontend/src/plugins/axios.js
+++ src/frontend/src/plugins/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({ baseURL: "http://localhost:3000/" });
 
-axiosInstance.interceptors.response.use((res) => res, (error) => {
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (error) => {
     const defaultMessage = "Возникла ошибка при выполнении запроса к серверу";
     axiosInstance.$notifier.error(
       error?.response?.data?.error?.message || defaultMessage
