c3f8bfd19dda19aeb85e30fe187d72d6f1e4b6cd
Mikhail Prokudin <mirex.doors@mail.ru>
2021-12-26 00:40:20 +0700

    add styles to radio input

--- src/frontend/src/common/components/AppRadioButton.vue
+++ src/frontend/src/common/components/AppRadioButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <label>
+  <label class="radio">
     <input
       :name="name"
       :value="itemValue"
@@ -35,3 +35,54 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+@import "~@/assets/scss/mixins/m_center.scss";
+.radio {
+  cursor: pointer;
+  span {
+    @include r-s16-h19;
+    position: relative;
+    padding-left: 28px;
+    &:before {
+      @include p_center-v;
+      display: block;
+      box-sizing: border-box;
+      width: 20px;
+      height: 20px;
+      content: "";
+      transition: 0.3s;
+      border: 1px solid $purple-400;
+      border-radius: 50%;
+      background-color: $white;
+    }
+  }
+  &:hover {
+    input:not(:checked):not(:disabled) + span {
+      &:before {
+        border-color: $purple-800;
+      }
+    }
+  }
+  input {
+    display: none;
+    &:checked + span {
+      &:before {
+        border: 6px solid $green-500;
+      }
+    }
+    &:disabled {
+      & + span {
+        &:before {
+          border-color: $purple-400;
+          background-color: $silver-200;
+        }
+      }
+      &:checked + span {
+        &:before {
+          border: 6px solid $purple-400;
+        }
+      }
+    }
+  }
+}
+</style>
