ca5bcb74ef97010f190ae65197cec6db2ed57e90
Mikhail Prokudin <mirex.doors@mail.ru>
2021-12-29 02:59:56 +0700

    finish split styles by components

--- src/frontend/src/modules/orders/OrderItem.vue
+++ src/frontend/src/modules/orders/OrderItem.vue
@@ -15,24 +15,15 @@
       </div>
 
       <div class="order__button">
-        <button
-          type="button"
-          class="button button--border"
-          @click="removeOrder(order.id)"
-        >
+        <AppButton mod-border @click="removeOrder(order.id)">
           Удалить
-        </button>
+        </AppButton>
       </div>
 
       <div class="order__button">
-        <button
-          type="button"
-          class="button"
-          data-test="repeat-order"
-          @click="handleRepeatOrder"
-        >
+        <AppButton data-test="repeat-order" @click="handleRepeatOrder">
           Повторить
-        </button>
+        </AppButton>
       </div>
     </div>
 
@@ -109,9 +100,11 @@
 <script>
 import { mapGetters, mapActions } from "vuex";
 import { REMOVE_ORDER, REPEAT_ORDER } from "@/store/actions.types";
+import AppButton from "@/common/components/AppButton";
 
 export default {
   name: "OrderItem",
+  components: { AppButton },
   props: {
     order: {
       type: Object,
@@ -200,3 +193,105 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+@import "~@/assets/scss/mixins/m_clear-list.scss";
+.order {
+  margin-bottom: 32px;
+  padding-top: 0;
+}
+.order__wrapper {
+  display: flex;
+  align-items: center;
+  padding: 6px 16px;
+  border-bottom: 1px solid rgba($green-500, 0.1);
+  b {
+    @include b-s14-h16;
+  }
+  span {
+    @include b-s14-h16;
+  }
+  button {
+    padding: 8px 26px;
+  }
+}
+.order__number {
+  margin-right: auto;
+}
+.order__sum {
+  margin-right: 16px;
+}
+.order__button {
+  margin-left: 16px;
+}
+.order__list {
+  @include clear-list;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  margin-top: 24px;
+  padding-right: 10px;
+  padding-left: 10px;
+}
+.order__item {
+  display: flex;
+  width: 310px;
+  margin-right: 33px;
+  margin-bottom: 32px;
+}
+.order__price {
+  @include b-s16-h19;
+  margin: 0;
+  white-space: nowrap;
+}
+.order__additional {
+  @include clear-list;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  margin-bottom: 5px;
+  padding-left: 80px;
+  li {
+    @include b-s11-h16;
+    width: 130px;
+    margin-right: 24px;
+    margin-bottom: 10px;
+  }
+  p {
+    margin: 0;
+  }
+  img {
+    float: left;
+    margin-right: 7px;
+  }
+  b {
+    display: block;
+  }
+}
+.order__address {
+  @include l-s11-h13;
+  margin: 0;
+  padding: 16px 10px;
+  border-top: 1px solid rgba($green-500, 0.1);
+}
+
+.product {
+  display: flex;
+  align-items: center;
+}
+
+.product__text {
+  margin-left: 15px;
+
+  h2 {
+    @include b-s18-h21;
+
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+
+  ul {
+    @include clear-list;
+    @include l-s11-h13;
+  }
+}
+</style>
--- src/frontend/src/modules/profile/ProfileAddressForm.vue
+++ src/frontend/src/modules/profile/ProfileAddressForm.vue
@@ -13,104 +13,89 @@
 
     <div class="address-form__wrapper">
       <div class="address-form__input">
-        <label class="input">
-          <span>Название адреса*</span>
-
-          <input
-            v-model="name"
-            type="text"
-            name="addr-name"
-            data-test="profile-address-name"
-            placeholder="Введите название адреса"
-            required
-          />
-        </label>
+        <AppInput
+          v-model="name"
+          type="text"
+          name="addr-name"
+          data-test="profile-address-name"
+          placeholder="Введите название адреса"
+          required
+        >
+          Название адреса*
+        </AppInput>
       </div>
 
       <div class="address-form__input address-form__input--size--normal">
-        <label class="input">
-          <span>Улица*</span>
-
-          <input
-            v-model="street"
-            type="text"
-            name="addr-street"
-            placeholder="Введите название улицы"
-            required
-            data-test="profile-address-street"
-          />
-        </label>
+        <AppInput
+          v-model="street"
+          type="text"
+          name="addr-street"
+          placeholder="Введите название улицы"
+          required
+          data-test="profile-address-street"
+        >
+          Улица*
+        </AppInput>
       </div>
 
       <div class="address-form__input address-form__input--size--small">
-        <label class="input">
-          <span>Дом*</span>
-
-          <input
-            v-model="building"
-            type="text"
-            name="addr-house"
-            placeholder="Введите номер дома"
-            required
-            data-test="profile-address-building"
-          />
-        </label>
+        <AppInput
+          v-model="building"
+          type="text"
+          name="addr-house"
+          placeholder="Введите номер дома"
+          required
+          data-test="profile-address-building"
+        >
+          Дом*
+        </AppInput>
       </div>
 
       <div class="address-form__input address-form__input--size--small">
-        <label class="input">
-          <span>Квартира</span>
-
-          <input
-            v-model="flat"
-            type="text"
-            name="addr-apartment"
-            placeholder="Введите № квартиры"
-            data-test="profile-address-flat"
-          />
-        </label>
+        <AppInput
+          v-model="flat"
+          type="text"
+          name="addr-apartment"
+          placeholder="Введите № квартиры"
+          data-test="profile-address-flat"
+        >
+          Квартира
+        </AppInput>
       </div>
 
       <div class="address-form__input">
-        <label class="input">
-          <span>Комментарий</span>
-
-          <input
-            v-model="comment"
-            type="text"
-            name="addr-comment"
-            placeholder="Введите комментарий"
-            data-test="profile-address-comment"
-          />
-        </label>
+        <AppInput
+          v-model="comment"
+          type="text"
+          name="addr-comment"
+          placeholder="Введите комментарий"
+          data-test="profile-address-comment"
+        >
+          Комментарий
+        </AppInput>
       </div>
     </div>
 
     <div class="address-form__buttons">
-      <button
-        type="button"
+      <AppButton
+        mod-transparent
         data-test="profile-address-form-btn-remove"
-        class="button button--transparent"
         @click="remove"
       >
         Удалить
-      </button>
+      </AppButton>
 
-      <button
-        :disabled="isDisabled"
-        type="submit"
-        :class="{ 'button-disabled': isDisabled }"
-        class="button"
-      >
-        Сохранить
-      </button>
+      <AppButton :disabled="isDisabled" type="submit"> Сохранить </AppButton>
     </div>
   </form>
 </template>
 
 <script>
+import AppButton from "@/common/components/AppButton";
+import AppInput from "@/common/components/AppInput";
 export default {
   name: "ProfileAddressForm",
+  components: { AppInput, AppButton },
   props: {
     number: {
       type: Number,
@@ -158,3 +143,66 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.address-form {
+  $bl: &;
+  position: relative;
+  padding-top: 0;
+  padding-bottom: 26px;
+  &--opened {
+    #{$bl}__header {
+      padding: 16px;
+    }
+  }
+  p {
+    @include r-s16-h19;
+    margin-top: 0;
+    margin-bottom: 16px;
+    padding: 0 16px;
+  }
+  small {
+    @include l-s11-h13;
+    display: block;
+    padding: 0 16px;
+  }
+}
+
+.address-form__wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 80%;
+  padding: 16px;
+}
+.address-form__input {
+  width: 100%;
+  margin-bottom: 16px;
+  &--size {
+    &--normal {
+      width: 60.5%;
+    }
+    &--small {
+      width: 18%;
+    }
+  }
+}
+.address-form__buttons {
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 16px;
+
+  .button {
+    margin-left: 16px;
+    padding: 16px 27px;
+  }
+}
+.address-form__header {
+  @include b-s14-h16;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 21px;
+  padding: 10px 16px;
+  border-bottom: 1px solid rgba($green-500, 0.1);
+}
+</style>
